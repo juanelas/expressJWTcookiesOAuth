26bc98eef30802d4aeb9c5c18e2639ad903bc8bd
Juan Hernández Serrano <jserrano@entel.upc.edu>
2019-05-24 11:35:48 +0200

    Fixed createUserFromGitHub() when some profile fields where missing

--- config/users.js
+++ config/users.js
@@ -19,8 +19,8 @@ const createUserFromGitHub = function (profile) {
     const user = {
         username: profile.username,
         password: null,
-        email: profile.emails[0].value,
-        name: profile.displayName
+        email: (profile.emails && profile.emails[0].value) ? profile.emails[0].value : null,
+        name: (profile.displayName) ? profile.displayName : profile.username
     };
     users.push(user);
     return user;
