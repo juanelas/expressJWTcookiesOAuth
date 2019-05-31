9153e1159d66520e19dd92938741677dfd0398bb
Juan Hernández Serrano <jserrano@entel.upc.edu>
2019-05-31 11:43:58 +0200

    Update config.js

--- config/tls/config.js
+++ config/tls/config.js
@@ -2,5 +2,5 @@ const path = require('path');
 
 module.exports = {
     certificate: path.join(__dirname, 'cert.pem'),
-    privateKey: path.join(__dirname, './privkey.pem')
+    privateKey: path.join(__dirname, 'privkey.pem')
 };
