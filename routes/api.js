6bc5944ec1f84cde22bed2c725caae8656a7b870
Juan Hernández Serrano <jserrano@entel.upc.edu>
2019-06-19 19:21:51 +0200

    more tuned cors

--- routes/api.js
+++ routes/api.js
@@ -12,9 +12,9 @@ const router = express.Router();
  */
 const cors = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', require('../config/origins').spa);
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Allow', 'GET, POST, OPTIONS');
     next();
 };
 
