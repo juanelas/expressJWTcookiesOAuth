e47890728d8ba86ff0fe34e7ed0f1b9469238267
juanelas <j.hernandez@upc.edu>
2021-02-09 13:42:35 +0100

    updated

--- app.js
+++ app.js
@@ -7,20 +7,16 @@ const logger = require('morgan')
 const passport = require('./config/passport')
 
 const app = express()
-
 app.use(logger('dev')) // Log requests (GET, POST, ...)
 app.use(express.urlencoded({ extended: true })) // needed to retrieve html form fields
 app.use(cookieParser()) // needed to retrieve html form fields
-app.use(express.json()) // needed to parse input json
 app.use(passport.initialize()) // initialise the authentication middleware
 
 /**
  * Load routes
  */
-const webRoutes = require('./routes/index')
-app.use('/', webRoutes)
-const api = require('./routes/api')
-app.use('/api', api)
+const routes = require('./routes/index')
+app.use('/', routes)
 
 /**
  * Create HTTPs server.
@@ -28,7 +24,7 @@ app.use('/api', api)
 const https = require('https')
 const fs = require('fs')
 
-const tlsConfig = require('./config/tls/')
+const tlsConfig = require('./config/tls/config')
 const credentials = {
   key: fs.readFileSync(tlsConfig.privateKey),
   cert: fs.readFileSync(tlsConfig.certificate)
@@ -36,18 +32,16 @@ const credentials = {
 var server = https.createServer(credentials, app)
 
 /**
- * Listen on provided port, on all network interfaces.
+ * Listen on localhost 443/tcp
  */
-server.listen(8443)
+const port = 8443
+const addr = 'localhost'
+server.listen(port, addr)
 server.on('listening', onListening)
 
 /**
  * Event listener for HTTP server "listening" event.
  */
 function onListening () {
-  const addr = server.address()
-  const bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port
-  console.log('Listening on ' + bind)
+  console.log(`Listening on https://${addr}:${port}`)
 }
